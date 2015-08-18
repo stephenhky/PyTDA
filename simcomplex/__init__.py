101f0084f9b297e9aa8ea1f70b36cfa12e60aec9
stephenhky <cwin31>
2015-08-18 15:57:27 -0400

    introducing Alpha Complex

--- simcomplex/__init__.py
+++ simcomplex/__init__.py
@@ -1,4 +1,17 @@
 __author__ = 'stephenhky'
 
+def facesiter(simplex):
+    for i in range(len(simplex)):
+        yield simplex[:i]+simplex[(i+1):]
+
+def flattening_simplex(simplices):
+    for simplex in simplices:
+        for point in simplex:
+            yield point
+
+def get_allpoints(simplices):
+    return set(flattening_simplex(simplices))
+
 from simcomplex.abssimcomplex import SimplicialComplex
 from simcomplex.vrcomplex import VietorisRipsComplex
+from simcomplex.alphacomplex import AlphaComplex
--- simcomplex/alphacomplex.py
+++ simcomplex/alphacomplex.py
@@ -1,6 +1,53 @@
 __author__ = 'stephenhky'
 
+from scipy.spatial import Delaunay, distance
 from simcomplex import SimplicialComplex
+from simcomplex import facesiter, get_allpoints
+from operator import or_
+from functools import partial
+
+def contain_detachededges(simplex, distdict, epsilon):
+    if len(simplex)==2:
+        return (distdict[simplex[0], simplex[1]] > 2*epsilon)
+    else:
+        return reduce(or_, map(partial(contain_detachededges, distdict=distdict, epsilon=epsilon), facesiter(simplex)))
 
 class AlphaComplex(SimplicialComplex):
-    pass
+    def __init__(self, points, epsilon, labels=None, distfcn=distance.euclidean):
+        self.pts = points
+        self.labels = range(len(self.pts)) if labels==None or len(labels)!=len(self.pts) else labels
+        self.epsilon = epsilon
+        self.distfcn = distfcn
+        self.import_simplices(self.construct_simplices(self.pts, self.labels, self.epsilon, self.distfcn))
+
+    def calculate_distmatrix(self, points, labels, distfcn):
+        distdict = {}
+        for i in range(len(labels)):
+            for j in range(len(labels)):
+                distdict[(labels[i], labels[j])] = distfcn(points[i], points[j])
+        return distdict
+
+    def construct_simplices(self, points, labels, epsilon, distfcn):
+        delaunay = Delaunay(points)
+        delaunay_simplices = map(tuple, delaunay.simplices)
+        distdict = self.calculate_distmatrix(points, labels, distfcn)
+
+        simplices = []
+        for simplex in delaunay_simplices:
+            faces = list(facesiter(simplex))
+            detached = map(partial(contain_detachededges, distdict=distdict, epsilon=epsilon), faces)
+            if reduce(or_, detached):
+                if len(simplex)>2:
+                    for face, notkeep in zip(faces, detached):
+                        if not notkeep:
+                            simplices.append(face)
+            else:
+                simplices.append(simplex)
+        simplices = map(lambda simplex: tuple(sorted(simplex)), simplices)
+        simplices = list(set(simplices))
+
+        allpts = get_allpoints(simplices)
+        for point in (set(labels)-allpts):
+            simplices += [(point,)]
+
+        return simplices
