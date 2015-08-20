4ae2b7ce8e78b9b59dfb8f744c1e0aea925a4972
stephenhky <cwin31>
2015-08-20 17:25:10 -0400

    examples included

--- demo/examples.py
+++ demo/examples.py
@@ -4,3 +4,9 @@ import numpy as np
 
 # 2D points
 twoDpointseg1 = np.array([[0,0], [0,1], [1,0], [1,1.5], [2.2, 3.3], [0, 4], [5,1], [10, 10]])
+
+# circular ring
+ring = np.array([[np.cos(t), np.sin(t)] for t in np.linspace(0, 2*np.pi, 1001)])
+
+# spherical ball
+sphere = np.array([[np.sin(theta)*np.cos(phi), np.sin(theta)*np.sin(phi), np.cos(theta)] for theta in np.linspace(0, np.pi, 1001) for phi in np.linspace(0, 2*np.pi, 1001)])
