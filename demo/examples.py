36f1ee0e9a9870fd7b7383a2aed3ef35ce475b61
stephenhky <stephenhky@yahoo.com.hk>
2017-12-07 22:14:32 -0500

    example (ring) updated

--- demo/examples.py
+++ demo/examples.py
@@ -6,7 +6,7 @@ import numpy as np
 twoDpointseg1 = np.array([[0,0], [0,1], [1,0], [1,1.5], [2.2, 3.3], [0, 4], [5,1], [10, 10]])
 
 # circular ring
-ring = np.array([[np.cos(t), np.sin(t)] for t in np.linspace(0, 2*np.pi, 1001)])[1:]
+ring = np.array([np.random.normal(loc=1, scale=0.01)*np.array([np.cos(t), np.sin(t)]) for t in np.linspace(0, 2*np.pi, 101)])[1:]
 
 # spherical ball
 sphere = np.array([[np.sin(theta)*np.cos(phi), np.sin(theta)*np.sin(phi), np.cos(theta)] for theta in np.linspace(0.02, np.pi, 51) for phi in np.linspace(0.02, 2*np.pi, 51)])
